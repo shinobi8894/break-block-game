6d01f6d63a97f885909e6c13d1947124b552d011
goldstardev2002 <goldstardev2002@gmail.com>
2022-02-20 05:37:10 +0900

    Draw Function Completed

--- canvas.js
+++ canvas.js
@@ -1,6 +1,13 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+
+var x = canvas.width/2;
+var y = canvas.height-30;
+
+var dx = 2;
+var dy = -2;
+
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
@@ -19,3 +26,20 @@ ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
 ctx.stroke();
 ctx.closePath();
 
+ctx.beginPath();
+ctx.arc(50, 50, 10, 0, Math.PI*2);
+ctx.fillStyle = "#0095DD";
+ctx.fill();
+ctx.closePath();
+
+
+function draw() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    x += dx;
+    y += dy;
+}
+setInterval(draw, 10);
