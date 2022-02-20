e582a5df9dadc2c7e00c31cdf0b670ee4fed86b2
goldstardev2002 <goldstardev2002@gmail.com>
2022-02-21 03:52:53 +0900

    Draw Function completed

--- canvas.js
+++ canvas.js
@@ -32,14 +32,18 @@ ctx.fillStyle = "#0095DD";
 ctx.fill();
 ctx.closePath();
 
-
 function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+
+function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
-    x += dx;
-    y += dy;
 }
 setInterval(draw, 10);
